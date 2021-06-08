720627e66a3c96b2b8fd76584d24c6c98af37ebd
Pavel Anni <pavel.anni@gmail.com>
2021-06-08 10:17:40 -0400

    Switch main to checking release part

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/pavelanni/pkgname"
 )
 
-var pkgList = []string{
-	"pt-sans-fonts-20141121-18.fc33.noarch.rpm",
-	"python3-canberra-0-0.25.git88c53cd.fc33.noarch.rpm",
-	"python3-pycurl-7.43.0.5-6.fc33.x86_64.rpm",
-	"shim-x64-15-8.x86_64.rpm",
-	"skkdic-20200128-2.T1339.fc33.noarch.rpm",
-	"squashfs-tools-4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
-	"4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
-	"-4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
-	"2.20200513gitc570c61.fc33.x86_64.rpm",
-	".x86_64.rpm",
-	"x86_64.rpm",
-	".rpm",
-	"rpm",
-	"bash_5.0-6ubuntu1.1_amd64.deb",
-	"dpkg_1.19.7ubuntu3_amd64.deb",
-	"gcc-10-base_10.2.0-5ubuntu1~20.04_amd64.deb",
-	"libapt-pkg6.0_2.0.5_amd64.deb",
-}
-
 func main() {
-
-	for _, pname := range pkgList {
-		p, err := pkgname.Parse(pname)
-		if err != nil {
-			fmt.Println(pname, err)
-			continue
+	/*
+		pkgList := []string{
+			"pt-sans-fonts-20141121-18.fc33.noarch.rpm",
+			"python3-canberra-0-0.25.git88c53cd.fc33.noarch.rpm",
+			"python3-pycurl-7.43.0.5-6.fc33.x86_64.rpm",
+			"shim-x64-15-8.x86_64.rpm",
+			"skkdic-20200128-2.T1339.fc33.noarch.rpm",
+			"squashfs-tools-4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
+			"4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
+			"-4.4-2.20200513gitc570c61.fc33.x86_64.rpm",
+			"2.20200513gitc570c61.fc33.x86_64.rpm",
+			".x86_64.rpm",
+			"x86_64.rpm",
+			".rpm",
+			"rpm",
+			"bash_5.0-6ubuntu1.1_amd64.deb",
+			"dpkg_1.19.7ubuntu3_amd64.deb",
+			"gcc-10-base_10.2.0-5ubuntu1~20.04_amd64.deb",
+			"libapt-pkg6.0_2.0.5_amd64.deb",
 		}
-		fmt.Println(pname, p)
+
+		for _, pname := range pkgList {
+			p, err := pkgname.Parse(pname)
+			if err != nil {
+				fmt.Println(pname, err)
+				continue
+			}
+			fmt.Println(pname, p)
+		}
+	*/
+
+	// This is to test if every package name has the release part
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if p, err := pkgname.Parse(scanner.Text()); err != nil {
+			fmt.Println(scanner.Text(), len(p.Release))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
 	}
 }
